b874f1d69f73ab405f7b7e4ac01662574dc0936f
vinayaksingh930 <113895193+vinayaksingh930@users.noreply.github.com>
2023-02-17 09:03:26 +0530

    Update Half_Pyramid_of_Numbers.c

--- Half_Pyramid_of_Numbers.c
+++ Half_Pyramid_of_Numbers.c
@@ -2,7 +2,10 @@
 
 int main()
 {
-  for (int j=1;j<=5;j++){
+    int row;
+    printf("enter the the number of rows:");
+    scanf("%d",&row);
+  for (int j=1;j<=row;j++){
       for (int i=1;i<=j;i++){
       printf("%d",i);
   }
