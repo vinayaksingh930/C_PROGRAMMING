5ed196d48dc62669f9cf7aaff0cedd1d992c5773
vinayaksingh930 <113895193+vinayaksingh930@users.noreply.github.com>
2023-02-24 09:16:57 +0530

    Create Product_of_2.c

---
+++ Product_of_2.c
@@ -0,0 +1,15 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int a,b;
+    scanf("%d%d",&a,&b);
+    printf("%d",(a*b));
+    return 0;
+}
