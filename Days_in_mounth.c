3afaca4acbe065e5479795474ed4f49146fd913d
vinayaksingh930 <113895193+vinayaksingh930@users.noreply.github.com>
2023-02-24 09:17:44 +0530

    Create Days_in_mounth.c

---
+++ Days_in_mounth.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    int a;
+    scanf("%d",&a);
+
+    switch(a)
+    {
+        case 1: 
+            printf("31");
+            break;
+        case 2: 
+            printf("28");
+            break;
+        case 3: 
+            printf("31");
+            break;
+        case 4:printf("30");
+            break;
+        case 5: 
+            printf("31");
+            break;
+        case 6: 
+            printf("30");
+            break;
+        case 7: 
+            printf("31");
+            break;
+        case 8: 
+            printf("31");
+            break;
+        case 9: 
+            printf("30");
+            break;
+        case 10: 
+            printf("31");
+            break;
+        case 11: 
+            printf("30");
+            break;
+        case 12: 
+            printf("31");
+            break;
+        default: 
+            printf("Wrong month value.");
+    return 0;
+    }
+}
