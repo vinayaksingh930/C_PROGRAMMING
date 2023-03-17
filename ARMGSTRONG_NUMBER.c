c789b0a9f44e4b04ff21fd7eb83c5f27e821e6fc
vinayaksingh930 <113895193+vinayaksingh930@users.noreply.github.com>
2023-03-17 09:10:30 +0530

    Create ARMGSTRONG_NUMBER.c

---
+++ ARMGSTRONG_NUMBER.c
@@ -0,0 +1,23 @@
+#include<math.h>
+#include <stdio.h>
+
+int main()
+{
+     int n,x,rem,res=0,count;
+     printf("enter a number:");
+     scanf("%d",&n);
+     count=log10(n)+1;
+     x=n;
+     while(n!=0){
+         rem=n%10;
+         res+=pow(rem,count);
+         n/=10;
+     }
+    
+     if(x==res)
+     printf("armstron");
+     else
+     printf("not armoston");
+
+    return 0;
+}
