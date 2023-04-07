604d700986dd14394a5f73c9387eb7072e4221d9
vinayaksingh930 <113895193+vinayaksingh930@users.noreply.github.com>
2023-04-07 08:47:56 +0530

    Create 2nd_Largest_Array.c

---
+++ 2nd_Largest_Array.c
@@ -0,0 +1,30 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int n,j,isShorted=0,temp;
+    scanf("%d",&n);
+    int a[n];
+    for(j=0;j<n;j++)
+        scanf("%d",&a[j]);
+    for (int i=0;i < (n-1);i++){
+        isShorted=1;
+        for (int j=0;j<n-i-1;j++)
+        {
+            if(a[j]>a[j+1]){
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+                isShorted=0;
+            }
+        }
+        if(isShorted)
+        break;
+    }
+    
+    printf("the min of the array is %d\n",a[0]);
+    printf("the 2nd largest max of the array is %d",a[n-2]);
+
+    return 0;
+}
